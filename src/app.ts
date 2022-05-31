0c742cff6bc77e8322dd43afd7763d8823854cc4
Abdouramane <abdouwer93@gmail.com>
2022-05-31 14:59:15 +0200

    Occurance and Divisibility handled for thecharacter '3', TODO: Reproduce feature for character '5' & '7'

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,36 @@
-import * as Const from "./constants";
-import { startup } from "./startup";
-import { functions } from "./functions";
-const { GAMEOVER_TAG, POSITION_TAG, LOOP_CASE, CYCLES_TO_LOOP } = Const;
- 
-export function App(filename: string) {  
-    const { handleTeleporting, handleContext, getContext, directions } = functions(filename);   
-    let { map } = startup(filename);
-    const gameoverPositionIndex: number = map.indexOf(GAMEOVER_TAG);
-    let currentPositionIndex: number = map.indexOf(POSITION_TAG);
+var output = "";
 
-    let nextIdx: number = 0;
-    let directionIndex: number = 0;
+class ContainsThree {
+  response: string;
+  character: string;
+  constructor(respondBy: string, character: string) {
+    this.response = respondBy;
+    this.character = character;
+  }
 
-    let forwardMode: boolean = true;
-    let drunk:  boolean = false;
-    let shouldTeleport:  boolean = false;
-    let isLooped:  boolean | undefined;
-    let context = directions[directionIndex];
-    let outputList: string[] = [];
-    let arrayCurrentPositionIndex: number[] = [];
+  handleResponse(str: string) {
+    this.checkOccurance(str);
+    this.checkDivisibility(str);
+  }
 
-    const storeCurrentPositionIndex = (currentPositionIndex: number) => {
-        arrayCurrentPositionIndex.push(currentPositionIndex);
-        /** After  CYCLES_TO_LOOP occurence of a currentPositionIndex, we are in a loop case */
-        for(let i = 0; i < arrayCurrentPositionIndex.length; i++) {
-            let count = 0;
-            for(let j = 0; j < arrayCurrentPositionIndex.length; j++) {
-                if(arrayCurrentPositionIndex[i] === arrayCurrentPositionIndex[j]) count += 1;
-            }
-            if(count === CYCLES_TO_LOOP) {
-                isLooped = true;
-                break;
-            }   
-        }
-    }
-    const setState = ({ 
-            currentPositionIndex: newCurrentPositionIndex,
-            context: newContext,
-            directionIndex: newDirectionIndex, 
-            shouldTeleport: newShouldTeleport, 
-            forwardMode: newForwardMode,   
-            drunk: newDrunk,
-            map: newMap}: Const.IRespContext) => 
-        {
-
-        currentPositionIndex = newCurrentPositionIndex
-        context = newContext
-        directionIndex = newDirectionIndex
-        shouldTeleport = newShouldTeleport
-        forwardMode = newForwardMode
-        drunk = newDrunk
-        map = newMap
-
-        outputList.push(newContext.headTo);
-        
-        if (gameoverPositionIndex == newCurrentPositionIndex) {
-            /** Blunder died, tell its story */
-            isLooped = false;
-        }
-        storeCurrentPositionIndex(newCurrentPositionIndex);
-    }
-    /** We dectect the tag symbol at next step then move accordingly */ 
-    const main = () => {
-
-        if(shouldTeleport) { 
-            const {shouldTeleport: resShouldTeleport, currentPositionIndex: respCurrentPositionIndex} = handleTeleporting(currentPositionIndex, map);
-            shouldTeleport = resShouldTeleport;
-            currentPositionIndex = respCurrentPositionIndex;
-            return;
-        }
-        context = getContext(directionIndex);  
-
-        nextIdx = currentPositionIndex + context["step"];
-        
-        const res = handleContext(map[nextIdx], currentPositionIndex, directionIndex, shouldTeleport, forwardMode, drunk, nextIdx, map);
-        
-        if(res)
-            setState(res);
-    }
-
-    while(map[currentPositionIndex] !== GAMEOVER_TAG && isLooped === undefined) main() 
-    if (isLooped) { 
-        return LOOP_CASE;
-    } else {
-        return outputList;
-    }
+  checkOccurance(str: string) {
+    const occurances = str.split(this.character).length - 1;
+    for (let index = 0; index < occurances; index++) output += this.response;
+  }
+  checkDivisibility(str: string) {
+    if (Number(str) % Number(this.character) === 0) output += this.response;
+  }
 }
+
+const printOut = () => {
+  console.log(output);
+};
+
+const main = (str: string) => {
+  rules.map((rule) => rule.handleResponse(str));
+  printOut();
+};
+
+const rules = new Array(new ContainsThree("Foo", "3"));
+
+main("3");
--- src/filereader.ts
+++
@@ -1,5 +0,0 @@
-export function filereader(file:string) {   
-    var fs = require("fs");
-    var text = fs.readFileSync(`src/files/${file}`).toString('utf-8');
-    return text.split("\n");
-}
