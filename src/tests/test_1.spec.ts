8773149f22e59cfd504422e9f3f1d3ce4159c47a
Abdouramane <abdouwer93@gmail.com>
2022-06-07 21:14:15 +0200

    main function's input tested

--- src/tests/test_1.spec.ts
+++ src/tests/test_1.spec.ts
@@ -1,7 +1,7 @@
 import { compute } from "../app";
 
 describe("Should output string as it is if string isn't 3 or 5 or 7", () => {
-  it("Get the string 1 as input", () => {
+  it("Get the string 1 as compute's input", () => {
     expect(compute("1")).toBe("1");
   });
 });
